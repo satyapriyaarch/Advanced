7078fe67d9cc504621a42aedc6e0216de0e7cc58
satyapriyaarch <30549744+satyapriyaarch@users.noreply.github.com>
2020-01-17 23:25:41 +0530

    ++

---
+++ Playgrounds/MyPlayground001.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Math
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+func reverse(one:Int,two:Int) -> Bool
+{
+    if one < two
+    {
+        return false
+    }
+    return true
+}
+ 
+sort(arr,reverse)
